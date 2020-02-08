0c20b470ce01061fb78ad6975912fdb564e132c0
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 07:07:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,16 +7,16 @@ class Artist
   def initialize(name)
     @name = name 
      
-    save 
+    # save 
   end 
   
   def self.all 
     @@all
   end   
   
-  def save 
-    @@all << self 
-  end   
+  # def save 
+  #   @@all << self 
+  # end   
   
   def songs 
     Song.all.select {|song| song.artist == self}
