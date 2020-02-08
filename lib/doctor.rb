a4d53f0410a8478eacbb6e95d9a0184a388eb110
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 07:08:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,17 +6,17 @@ class Doctor
   
   def initialize(name)
     @name = name 
-    
-    save
+    @@all << self
+    # save
   end   
   
   def self.all
     @@all 
   end
   
-  def save
-    @@all << self
-  end   
+  # def save
+  #   @@all << self
+  # end   
   
   def new_appointment(patient, date)
     Appointment.new(date, patient, self)
